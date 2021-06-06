aa6c77572ba004b73eb4ba33c571205896d82db1
Tugrul Alpdogan <tugrul@alpdogan.co>
2021-06-06 23:07:41 +0300

    protection from laws :)

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 5151
-const SITE_URL = 'https://www.dominos.com.tr';
+const SITE_URL = 'https://www.example.com';
 
 app.get('*', (req, res) => {
     console.log(req.url);
